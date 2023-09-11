e4465a6cce958e61c2e0a5ff5dd6c87059f0d6b7
BrageDrage909 <bfu01.bb@gmail.com>
2023-09-11 10:44:57 +0200

    update

--- javascript/main.js
+++ javascript/main.js
@@ -1,24 +1,28 @@
-question1 = document.querySelector(".question-1")
-
-let question = parseInt(localStorage.getItem("question"));
-
-if (isNaN(question)) {
-    question = 1;
-    localStorage.setItem("question", question);
-}
+let currentQuestion = 1; 
 
 function next() {
-    if (question < 10) {
-        question += 1;
-        localStorage.setItem('question', question);
-    }
+  if (currentQuestion < 10) {
+    document.querySelector(`.question-${currentQuestion}`).style.display = 'none';
+    currentQuestion++;
+    document.querySelector(`.next-button-${currentQuestion}`).style.display = 'block';;
+  };      
 }
 
 function previous() {
-    if (question > 1) {
-        question -= 1;
-        localStorage.setItem('question', question);
-    }
+  if (currentQuestion > 1) {
+    document.querySelector(`.question-${currentQuestion}`).style.display = 'none';
+    currentQuestion--;
+    document.querySelector(`.question-${currentQuestion}`).style.display = 'block';
+  }
 }
+
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+
+radioButtons.forEach(function (radioButton) {
+  radioButton.addEventListener('change', function () {
+      const button = document.getElementById(`next-button-${currentQuestion}`);
+      button.removeAttribute('disabled');
+    });
+});
 
 
